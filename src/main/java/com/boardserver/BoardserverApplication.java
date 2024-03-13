053bc50a7de49901fa8e71f7d9c8cc60d23edc3e
jungwoonson <jungwoon727@gmail.com>
2024-03-13 19:09:43 +0900

    refactor: BoardserverApplication.java 위치 변경

--- src/main/java/com/boardserver/BoardserverApplication.java
+++ src/main/java/com/boardserver/BoardserverApplication.java
@@ -1,4 +1,4 @@
-package com.toy.boardserver.boardserver;
+package com.boardserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/boardserver/BoardserverApplicationTests.java
+++ src/test/java/com/boardserver/BoardserverApplicationTests.java
@@ -1,4 +1,4 @@
-package com.toy.boardserver.boardserver;
+package com.boardserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
