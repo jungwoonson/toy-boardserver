da7e852422dda51aebad8e692bd2cc35bfa4f644
jungwoonson <jungwoon727@gmail.com>
2024-03-13 19:41:54 +0900

    feat: user 관련 클래스 생성

---
+++ src/main/java/com/boardserver/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.boardserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+
+    public enum Status {
+        DEFAULT, ADMIN, DELETED
+    }
+
+    private int id;
+    private String userId;
+    private String password;
+    private String nickname;
+    private boolean isAdmin;
+    private Date createTime;
+    private boolean isWithDraw;
+    private Status status;
+    private Date updateTime;
+}
